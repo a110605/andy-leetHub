d10f3436c795081a58ff9c8a29390a539062abcd
Andy Lee <a1106052000@gmail.com>
2024-01-06 23:51:59 +0800

    Time: 48 ms (88.8%), Space: 41.7 MB (91.87%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    if(strs === null || strs.length === 0) return "";
+    
+    let prefix = strs[0]; // assume first word is common prefix
+    // compare to following words
+    for(let i = 1 ; i < strs.length; i++){
+        while(strs[i].indexOf(prefix) !== 0){
+            prefix = prefix.substring(0, prefix.length - 1);
+        }
+    }
+    return prefix;
+};
